7c148278130efe95dc1a2680b77cf2c7a91040d5
Sebastian Staudt <koraktor@gmail.com>
2008-07-27 08:53:48 +0000

    (Ruby) Added a module for request packets with challenge number

--- ruby/lib/steam/packets/a2a_player_request_packet.rb
+++ ruby/lib/steam/packets/a2a_player_request_packet.rb
@@ -1,16 +1,15 @@
+autoload "RequestWithChallenge", "steam/packets/request_with_challenge"
 autoload "SteamPacket", "steam/packets/steam_packet"
 
 # The A2A_PING_RequestPacket class represents a A2A_PING request send to the
 # server.
 class A2A_PLAYER_RequestPacket < SteamPacket
   
+  include RequestWithChallenge
+  
   # Creates a new A2A_PLAYER request object including the challenge_number
   def initialize(challenge_number = "FFFFFFFF".to_i(16))
     super SteamPacket::A2A_PLAYER_REQUEST_HEADER, challenge_number
   end
   
-  # Returns a packed version of the A2A_PLAYER request
-  def to_s
-    return [0xFF, 0xFF, 0xFF, 0xFF, @header_data, @content_data].pack("c5l")
-  end
 end
---
+++ ruby/lib/steam/packets/request_with_challenge.rb
@@ -0,0 +1,9 @@
+# This module implements a to_s method for request packets which send a challenge number
+module RequestWithChallenge
+  
+  # Returns a packed version of the request
+  def to_s
+    return [0xFF, 0xFF, 0xFF, 0xFF, @header_data, @content_data].pack("c5l")
+  end
+  
+end
