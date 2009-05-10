aab8e28e6e43a807d5e96f0dcdd6b819cac6ffa1
Sebastian Staudt <koraktor@gmail.com>
2009-05-10 15:41:38 +0200

    Ruby: Changed SteamCondenserException to subclass StandardError
    This prevents IRB from exiting on errors.

--- ruby/lib/exceptions/steam_condenser_exception.rb
+++ ruby/lib/exceptions/steam_condenser_exception.rb
@@ -1,10 +1,8 @@
 # This code is free software; you can redistribute it and/or modify it under the
 # terms of the new BSD License.
 #
-# Copyright (c) 2008, Sebastian Staudt
-#
-# $Id$
+# Copyright (c) 2008-2009, Sebastian Staudt
 
-class SteamCondenserException < Exception
+class SteamCondenserException < StandardError
   
 end
