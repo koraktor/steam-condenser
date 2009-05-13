393b17afdf1aceb40fd5bd8a30d148b8d6eb161e
Sebastian Staudt <koraktor@gmail.com>
2009-05-13 14:34:50 +0200

    Ruby: Changes to DoDSWeapon
    * Fixed usage of @shots_fired (renamed to @shots)
    * Renamed @shots_hit to @hits

--- ruby/lib/steam/community/dods/dods_weapon.rb
+++ ruby/lib/steam/community/dods/dods_weapon.rb
@@ -5,9 +5,10 @@
 
 require 'steam/community/game_weapon'
 
+# Represents the stats for a Day of Defeat: Source weapon for a specific user
 class DoDSWeapon < GameWeapon
 
-  attr_reader :headshots, :name, :shots_hit
+  attr_reader :headshots, :name, :hits
 
   # Creates a new instance of DoDSWeapon based on the assigned XML data
   def initialize(weapon_data)
@@ -17,37 +18,37 @@ class DoDSWeapon < GameWeapon
     @id        = weapon_data.attributes['key']
     @name      = weapon_data.elements['name'].text
     @shots     = weapon_data.elements['shotsfired'].text.to_i
-    @shots_hit = weapon_data.elements['shotshit'].text.to_i
+    @hits      = weapon_data.elements['shotshit'].text.to_i
   end
 
   # Returns the average number of hits needed for a kill with this weapon.
   # Calculates the value if needed.
   def avg_hits_per_kill
     if @avg_hits_per_kill.nil?
-      @avg_hits_per_kill = @shots_hit.to_f / @kills
+      @avg_hits_per_kill = @hits.to_f / @kills
     end
 
     @avg_hits_per_kill
-  end
-
-  # Returns the percentage of hits relative to the shots fired with this
-  # weapon. Calculates the value if needed.
-  def hit_percentage
-    if @hit_percentage.nil?
-      @hit_percentage = @shots_hit.to_f / @shots_fired
-    end
-
-    @hit_percentage
   end
 
   # Returns the percentage of headshots relative to the shots hit with this
   # weapon. Calculates the value if needed.
   def headshot_percentage
     if @headshot_percentage.nil?
-      @headshot_percentage = @headshots.to_f / @shots_hit
+      @headshot_percentage = @headshots.to_f / @hits
     end
 
     @headshot_percentage
+  end
+
+  # Returns the percentage of hits relative to the shots fired with this
+  # weapon. Calculates the value if needed.
+  def hit_percentage
+    if @hit_percentage.nil?
+      @hit_percentage = @hits.to_f / @shots
+    end
+
+    @hit_percentage
   end
 
 end
