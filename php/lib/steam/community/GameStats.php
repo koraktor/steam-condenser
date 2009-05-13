ff55857e76b0dcd9635f8058557e7b87dc40d0fe
Sebastian Staudt <koraktor@gmail.com>
2009-05-13 13:01:09 +0200

    PHP: Updated GameStats and TF2Stats
    * Moved $accumulatedPoints to TF2Stats as this is specific to TF2
    * Added support for HTTP proxies
    * Reformatted code

--- php/lib/steam/community/GameStats.php
+++ php/lib/steam/community/GameStats.php
@@ -11,7 +11,7 @@
  */
 
 require_once "steam/community/GameAchievement.php";
-require_once "steam/community/TF2Stats.php";
+require_once "steam/community/tf2/TF2Stats.php";
 
 /**
  * The GameStats class represents the game statistics for a single user and a
@@ -19,68 +19,66 @@ require_once "steam/community/TF2Stats.php";
  * @package Steam Condenser (PHP)
  * @subpackage Steam Community
  */
-class GameStats
-{
-	/**
-	 * Used to cache the XML data of the statistics for this game and this user
-	 * @var SimpleXML
-	 */
-	private $xmlData;
+class GameStats {
 
-        /**
-         *
-         */
-        public static function createGameStats($steamId, $gameName)
-        {
-            switch($gameName) {
-                case "TF2":
-                    return new TF2Stats($steamId, $gameName);
-                default:
-                    return new GameStats($steamId, $gameName);
-            }
+    /**
+     * Used to cache the XML data of the statistics for this game and this
+     * user
+     * @var SimpleXML
+     */
+    protected $xmlData;
+
+    /**
+     * Creates a GameStats (or one of its subclasses) object for the given user
+     * depending on the game selected
+     */
+    public static function createGameStats($steamId, $gameName) {
+        switch($gameName) {
+            case "TF2":
+                return new TF2Stats($steamId);
+            default:
+                return new GameStats($steamId, $gameName);
         }
+    }
 
 	/**
-	 * Creates a GameStats object and fetchs data from Steam Community for the
-	 * given user and game
+	 * Creates a GameStats object and fetchs data from Steam Community for
+         * the given user and game
 	 * @param $steamId
 	 * @param $gameName
 	 */
-	protected function __construct($steamId, $gameName)
-	{
-		$this->steamId = $steamId;
+    protected function __construct($steamId, $gameName) {
+        $this->steamId = $steamId;
 
-		$this->xmlData = new SimpleXMLElement("http://www.steamcommunity.com/id/{$this->steamId}/stats/{$this->gameName}?xml=1", null, true);
+        $this->xmlData = new SimpleXMLElement(file_get_contents("http://www.steamcommunity.com/id/{$this->steamId}/stats/{$gameName}?xml=1"));
 
-		$this->privacyState = $this->xmlData->privacyState;
-		if($this->privacyState == "public")
-		{
-			$this->accumulatedPoints = intval($this->xmlData->stats->accumulatedPoints);
-			$this->appId = intval($this->xmlData);
-			$this->gameFriendlyName = $this->gameFriendlyName;
-			$this->gameName = $this->gameName;
-			$this->hoursPlayed = floatval($this->hoursPlayed);
-		}
-	}
+        $this->privacyState = (string) $this->xmlData->privacyState;
+        if($this->privacyState == 'public') {
+            preg_match('#http://store.steampowered.com/app/([1-9][0-9]*)#', (string) $this->xmlData->game->gameLink, $appId);
+            $this->appId = (int) $appId[1];
+            $this->gameFriendlyName = (string) $this->xmlData->game->gameFriendlyName;
+            $this->gameName = (string) $this->xmlData->game->gameName;
+            $this->hoursPlayed = (string) $this->xmlData->stats->hoursPlayed;
+        }
+    }
 
 	/**
 	 * Returns the achievements for this stats' user and game. If the achievements
 	 * haven't been parsed already, parsing is done now.
 	 * @return GameAchievements[]
 	 */
-	public function getAchievements()
-	{
-		if(empty($this->achievements)) {
-                        $this->achievementsDone = 0;
-			foreach($this->xmlData->achievements as $achievement) {
-				$this->achievements[] = new GameAchievement($this->steamId, $this->appId, $achievement->name, ($achievement->closed == 1));
-                                if($achievement->closed == 1) {
-                                    $this->achievementsDone += 1;
-                                }
-			}
-		}
+    public function getAchievements() {
+        if(empty($this->achievements)) {
+            $this->achievementsDone = 0;
+            foreach($this->xmlData->achievements as $achievement) {
+                $this->achievements[] = new GameAchievement($this->steamId, $this->appId, $achievement->name, ($achievement->closed == 1));
+                if($achievement->closed == 1) {
+                    $this->achievementsDone += 1;
+                }
+            }
+        }
 
-		return $this->achievements;
-	}
+        return $this->achievements;
+    }
 }
 ?>
--- php/lib/steam/community/tf2/TF2Stats.php
+++ php/lib/steam/community/tf2/TF2Stats.php
@@ -3,14 +3,16 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
+ * Copyright (c) 2008-2009, Sebastian Staudt
+ *
  * @author Sebastian Staudt
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @package Steam Condenser (PHP)
  * @subpackage Steam Community
- * @version $Id$
  */
 
 require_once "steam/community/GameStats.php";
+require_once "steam/community/tf2/TF2Class.php";
 
 /**
  * The TF2Stats class represents the game statistics for a single user in Team
@@ -18,33 +20,33 @@ require_once "steam/community/GameStats.php";
  * @package Steam Condenser (PHP)
  * @subpackage Steam Community
  */
-class TF2Stats extends GameStats
-{
-	/**
-	 * Creates a TF2Stats object by calling the super constructor with the game
-	 * name "TF2"
-	 * @var $steamId
-	 */
-	public function __construct($steamId)
-	{
-		parent::__construct($steamId, "TF2");
-	}
+class TF2Stats extends GameStats {
 
-	/**
-	 * Returns a Hash of TF2Class for this user containing all Team Fortress 2
-	 * classes. If the classes haven't been parsed already, parsing is done now.
-	 */
-	public function getClassStats()
-	{
-		if(empty($this->classStats))
-		{
-			foreach($this->xmlData->stats->classData as $classData)
-			{
-				$this->classStats["className"] = new TF2Class($classData);
-			}
-		}
+    /**
+     * Creates a TF2Stats object by calling the super constructor with the game
+     * name "TF2"
+     * @param $steamId
+     */
+    public function __construct($steamId) {
+        parent::__construct($steamId, "TF2");
 
-		return $this->classStats;
-	}
+        if($this->privacyState == 'public') {
+            $this->accumulatedPoints = intval($this->xmlData->stats->accumulatedPoints);
+        }
+    }
+
+    /**
+     * Returns a Hash of TF2Class for this user containing all Team Fortress 2
+     * classes. If the classes haven't been parsed already, parsing is done now.
+     */
+    public function getClassStats() {
+        if(empty($this->classStats)) {
+            foreach($this->xmlData->stats->classData as $classData) {
+                $this->classStats[$classData->className] = new TF2Class($classData);
+            }
+        }
+
+        return $this->classStats;
+    }
 }
 ?>
