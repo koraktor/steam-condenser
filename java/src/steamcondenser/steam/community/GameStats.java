79c4b369616d37ec994fb13d3322b30a8e947edf
Sebastian Staudt <koraktor@gmail.com>
2009-05-17 18:20:02 +0200

    Java: Moved attribute accumulatedPoints to TF2Stats as this is specific to GameStats
    * Added isPublic() method to GameStats
    * Reformatted code

--- java/src/steamcondenser/steam/community/GameStats.java
+++ java/src/steamcondenser/steam/community/GameStats.java
@@ -1,7 +1,10 @@
 /** 
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
+ *
+ * Copyright (c) 2008-2009, Sebastian Staudt
  */
+
 package steamcondenser.steam.community;
 
 import java.io.IOException;
@@ -21,122 +24,110 @@ import steamcondenser.steam.community.tf2.TF2Stats;
  * The GameStats class represents the game statistics for a single user and a
  * specific game
  * @author Sebastian Staudt
- * @version $Id$
  */
-public class GameStats
-{
-    protected int accumulatedPoints;
-    protected ArrayList<GameAchievement> achievements;
-    protected int appId;
-    protected String customUrl;
-    protected String gameFriendlyName;
-    protected String gameName;
-    protected float hoursPlayed;
-    protected String privacyState;
-    protected long steamId64;
-    protected Element xmlData;
+public class GameStats {
 
-    public static GameStats createGameStats(String customUrl, String gameName)
-            throws ParserConfigurationException, SAXException, IOException
-    {
-        if(gameName.equals("TF2")) {
-            return new TF2Stats(customUrl);
-        }
-        else {
-            return new GameStats(customUrl, gameName);
-        }
-    }
+	protected ArrayList<GameAchievement> achievements;
+	protected int appId;
+	protected String customUrl;
+	protected String gameFriendlyName;
+	protected String gameName;
+	protected float hoursPlayed;
+	protected String privacyState;
+	protected long steamId64;
+	protected Element xmlData;
 
-    public static GameStats createGameStats(long steamId64, String gameName)
-            throws ParserConfigurationException, SAXException, IOException
-    {
-        if(gameName.equals("TF2")) {
-            return new TF2Stats(steamId64);
-        }
-        else {
-            return new GameStats(steamId64, gameName);
-        }
-    }
+	public static GameStats createGameStats(String customUrl, String gameName)
+			throws ParserConfigurationException, SAXException, IOException {
+		if(gameName.equals("TF2")) {
+			return new TF2Stats(customUrl);
+		} else {
+			return new GameStats(customUrl, gameName);
+		}
+	}
 
-    protected GameStats(String customUrl, String gameName)
-            throws ParserConfigurationException, SAXException, IOException
-    {
-        this.customUrl = customUrl;
-        this.gameName = gameName;
-        this.fetch();
-    }
+	public static GameStats createGameStats(long steamId64, String gameName)
+			throws ParserConfigurationException, SAXException, IOException {
+		if(gameName.equals("TF2")) {
+			return new TF2Stats(steamId64);
+		} else {
+			return new GameStats(steamId64, gameName);
+		}
+	}
 
-    protected GameStats(long steamId64, String gameName)
-            throws ParserConfigurationException, SAXException, IOException
-    {
-        this.steamId64 = steamId64;
-        this.gameName = gameName;
-        this.fetch();
-    }
+	protected GameStats(String customUrl, String gameName)
+			throws ParserConfigurationException, SAXException, IOException {
+		this.customUrl = customUrl;
+		this.gameName  = gameName;
+		this.fetch();
+	}
 
-    protected void fetch()
-            throws ParserConfigurationException, SAXException, IOException
-    {
-        String url;
-        if(this.customUrl == null) {
-            url = "http://www.steamcommunity.com/id/" + this.customUrl + "/stats/" + this.gameName + "?xml=1";
-        }
-        else {
-            url = "http://www.steamcommunity.com/id/" + this.steamId64 + "/stats/" + this.gameName + "?xml=1";
-        }
+	protected GameStats(long steamId64, String gameName)
+			throws ParserConfigurationException, SAXException, IOException {
+		this.steamId64 = steamId64;
+		this.gameName  = gameName;
+		this.fetch();
+	}
 
-        DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        this.xmlData = parser.parse(url).getDocumentElement();
+	protected void fetch()
+			throws ParserConfigurationException, SAXException, IOException {
+		String url;
+		if(this.customUrl == null) {
+			url = "http://www.steamcommunity.com/id/" + this.customUrl + "/stats/" + this.gameName + "?xml=1";
+		} else {
+			url = "http://www.steamcommunity.com/id/" + this.steamId64 + "/stats/" + this.gameName + "?xml=1";
+		}
 
-        this.privacyState = this.xmlData.getElementsByTagName("privacyState").item(0).getTextContent();
-        if(this.privacyState.equals("public")) {
-            this.accumulatedPoints = Integer.parseInt(((Element)this.xmlData.getElementsByTagName("stats").item(0)).getElementsByTagName("accumulatedPoints").item(0).getTextContent());
-            this.appId = Integer.parseInt(((Element)this.xmlData.getElementsByTagName("game").item(0)).getElementsByTagName("gameLink").item(0).getTextContent().replace("http://store.steampowered.com/app/", ""));
-            this.gameFriendlyName = ((Element)this.xmlData.getElementsByTagName("game").item(0)).getElementsByTagName("gameFriendlyName").item(0).getTextContent();
-            this.gameName = ((Element)this.xmlData.getElementsByTagName("game").item(0)).getElementsByTagName("gameName").item(0).getTextContent();
-            this.hoursPlayed = Float.parseFloat(((Element)this.xmlData.getElementsByTagName("stats").item(0)).getElementsByTagName("hoursPlayed").item(0).getTextContent());
-        }
-    }
+		DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		this.xmlData = parser.parse(url).getDocumentElement();
 
-    public int getAccumulatedPoints()
-    {
-        return this.accumulatedPoints;
-    }
+		this.privacyState = this.xmlData.getElementsByTagName("privacyState").item(0).getTextContent();
+		if(this.isPublic()) {
+			this.appId            = Integer.parseInt(((Element) this.xmlData.getElementsByTagName("game").item(0)).getElementsByTagName("gameLink").item(0).getTextContent().replace("http://store.steampowered.com/app/", ""));
+			this.gameFriendlyName = ((Element) this.xmlData.getElementsByTagName("game").item(0)).getElementsByTagName("gameFriendlyName").item(0).getTextContent();
+			this.gameName         = ((Element) this.xmlData.getElementsByTagName("game").item(0)).getElementsByTagName("gameName").item(0).getTextContent();
+			this.hoursPlayed      = Float.parseFloat(((Element) this.xmlData.getElementsByTagName("stats").item(0)).getElementsByTagName("hoursPlayed").item(0).getTextContent());
+		}
+	}
 
-    public ArrayList<GameAchievement> getAchievements()
-    {
-        if(this.achievements == null) {
-            this.achievements = new ArrayList<GameAchievement>();
+	/**
+	 * Returns the achievements for this stats' user and game. If the
+	 * achievements haven't been parsed already, parsing is done now.
+	 * @return
+	 */
+	public ArrayList<GameAchievement> getAchievements() {
+		if(this.achievements == null) {
+			this.achievements = new ArrayList<GameAchievement>();
 
-            NodeList achievementsNode = this.xmlData.getElementsByTagName("achievements").item(0).getChildNodes();
-            for(int i = 0; i < achievementsNode.getLength(); i ++) {
-                Element achievement = (Element)achievementsNode.item(i);
-                String achievementName = achievement.getElementsByTagName("name").item(0).getTextContent();
-                boolean achievementDone = achievement.getElementsByTagName("closed").item(0).getTextContent().equals("1");
-                if(this.customUrl == null) {
-                    this.achievements.add(new GameAchievement(this.appId, achievementName, achievementDone));
-                }
-                else {
-                    this.achievements.add(new GameAchievement(this.appId, achievementName, achievementDone));
-                }
-            }
-        }
+			NodeList achievementsNode = this.xmlData.getElementsByTagName("achievements").item(0).getChildNodes();
+			for(int i = 0; i < achievementsNode.getLength(); i++) {
+				Element achievement = (Element) achievementsNode.item(i);
+				String achievementName = achievement.getElementsByTagName("name").item(0).getTextContent();
+				boolean achievementDone = achievement.getElementsByTagName("closed").item(0).getTextContent().equals("1");
+				if(this.customUrl == null) {
+					this.achievements.add(new GameAchievement(this.appId, achievementName, achievementDone));
+				} else {
+					this.achievements.add(new GameAchievement(this.appId, achievementName, achievementDone));
+				}
+			}
+		}
 
-        return achievements;
-    }
+		return achievements;
+	}
 
-    public String getGameFriendlyName()
-    {
-        return this.gameFriendlyName;
-    }
+	public String getGameFriendlyName() {
+		return this.gameFriendlyName;
+	}
 
-    public String getGameName()
-    {
-        return this.gameName;
-    }
+	public String getGameName() {
+		return this.gameName;
+	}
 
-    public float getHoursPlayed()
-    {
-        return this.hoursPlayed;
-    }
+	public float getHoursPlayed() {
+		return this.hoursPlayed;
+	}
+
+	protected boolean isPublic() {
+		return this.privacyState.equals("public");
+	}
 }
--- java/src/steamcondenser/steam/community/tf2/TF2Stats.java
+++ java/src/steamcondenser/steam/community/tf2/TF2Stats.java
@@ -1,7 +1,10 @@
 /** 
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
+ *
+ * Copyright (c) 2008-2009, Sebastian Staudt
  */
+
 package steamcondenser.steam.community.tf2;
 
 import java.io.IOException;
@@ -20,55 +23,64 @@ import steamcondenser.steam.community.GameStats;
  * Fortress 2
  * 
  * @author Sebastian Staudt
- * @version $Id$
  */
-public class TF2Stats extends GameStats
-{
-    private ArrayList<TF2Class> classStats;
+public class TF2Stats extends GameStats {
 
-    /**
-     * Creates a new object holding Team Fortress 2 statistics for the given
-     * user
-     * @param steamId
-     * @throws ParserConfigurationException
-     * @throws SAXException
-     * @throws IOException
-     */
-    public TF2Stats(String customUrl)
-            throws ParserConfigurationException, SAXException, IOException
-    {
-        super(customUrl, "TF2");
-    }
+	private int accumulatedPoints;
+	private ArrayList<TF2Class> classStats;
 
-    /**
-     * Creates a new object holding Team Fortress 2 statistics for the given
-     * user
-     * @param steamId
-     * @throws ParserConfigurationException
-     * @throws SAXException
-     * @throws IOException
-     */
-    public TF2Stats(long steamId64)
-            throws ParserConfigurationException, SAXException, IOException
-    {
-        super(steamId64, "TF2");
-    }
+	/**
+	 * Creates a new object holding Team Fortress 2 statistics for the given
+	 * user
+	 * @param steamId
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
+	public TF2Stats(String customUrl)
+			throws ParserConfigurationException, SAXException, IOException {
+		super(customUrl, "TF2");
+		this.initialize();
+	}
 
-    /**
-     * Returns the statistics for all Team Fortress 2 classes for this user
-     * @return An array storing individual TF2Class objects for each Team
-     *         Fortress 2 class
-     */
-    public ArrayList<TF2Class> getClassStats()
-    {
-        if(this.classStats == null) {
-            this.classStats = new ArrayList<TF2Class>();
-            NodeList classes = ((Element)this.xmlData.getElementsByTagName("stats").item(0)).getElementsByTagName("classData");
-            for(int i = 0; i < classes.getLength(); i ++) {
-                this.classStats.add(TF2ClassFactory.getTF2Class((Element)classes.item(i)));
-            }
-        }
+	/**
+	 * Creates a new object holding Team Fortress 2 statistics for the given
+	 * user
+	 * @param steamId
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
+	public TF2Stats(long steamId64)
+			throws ParserConfigurationException, SAXException, IOException {
+		super(steamId64, "TF2");
+		this.initialize();
+	}
 
-        return this.classStats;
-    }
+	private void initialize() {
+		if(this.isPublic()) {
+			this.accumulatedPoints = Integer.parseInt(((Element) this.xmlData.getElementsByTagName("stats").item(0)).getElementsByTagName("accumulatedPoints").item(0).getTextContent());
+		}
+	}
+
+	public int getAccumulatedPoints() {
+		return this.accumulatedPoints;
+	}
+
+	/**
+	 * Returns the statistics for all Team Fortress 2 classes for this user
+	 * @return An array storing individual TF2Class objects for each Team
+	 *         Fortress 2 class
+	 */
+	public ArrayList<TF2Class> getClassStats() {
+		if(this.classStats == null) {
+			this.classStats = new ArrayList<TF2Class>();
+			NodeList classes = ((Element) this.xmlData.getElementsByTagName("stats").item(0)).getElementsByTagName("classData");
+			for(int i = 0; i < classes.getLength(); i++) {
+				this.classStats.add(TF2ClassFactory.getTF2Class((Element) classes.item(i)));
+			}
+		}
+
+		return this.classStats;
+	}
 }
