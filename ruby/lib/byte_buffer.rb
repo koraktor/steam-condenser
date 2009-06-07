koraktor/steam-condenser

commit 17f210c0fab50464ff6526807b2c91efc53efcf9

Ruby: Fixed problems with BZ2 compressed packets
* Changed ByteBuffer#get_long to return unsigned long integers. This fixes problems when checking the CRC32 checksum of compressed packets.
* Require Zlib and BZ2 modules

ruby/lib/byte_buffer.rb
@@ -79,7 +79,7 @@ class ByteBuffer
   end
   
   def get_long
-    return self.get(4).unpack("l")[0]
+    return self.get(4).unpack("V")[0]
   end
 
   def get_short

ruby/lib/steam/packets/steam_packet_factory.rb
@@ -1,9 +1,9 @@
 # This code is free software; you can redistribute it and/or modify it under the
 # terms of the new BSD License.
 #
-# Copyright (c) 2008, Sebastian Staudt
-#
-# $Id$
+# Copyright (c) 2008-2009, Sebastian Staudt
+
+require 'zlib'
 
 require "abstract_class"
 require "steam/packets/s2a_info_detailed_packet"
@@ -70,9 +70,10 @@ class SteamPacketFactory
     packet_data = split_packets.join ""
     
     if is_compressed
-      if defined? BZ2
+      begin
+        require 'bz2'
         packet_data = BZ2.uncompress(packet_data)
-      else
+      rescue LoadError
         raise SteamCondenserException.new("You need to install the libbzip2 interface for Ruby.")
       end
       

ruby/lib/steam/sockets/goldsrc_socket.rb
@@ -24,7 +24,7 @@ class GoldSrcSocket < SteamSocket
   def get_reply
     bytes_read = self.receive_packet 1400
     
-    if @buffer.get_long == -2
+    if @buffer.get_long == 0xFFFFFFFE
       split_packets = Array.new
       begin
         # Parsing of split packet headers
@@ -48,7 +48,7 @@ class GoldSrcSocket < SteamSocket
         else
           bytes_read = 0
         end
-      end while bytes_read > 0 && @buffer.get_long == -2
+      end while bytes_read > 0 && @buffer.get_long == 0xFFFFFFFE
       
       packet = SteamPacketFactory.reassemble_packet(split_packets)
         

ruby/lib/steam/sockets/source_socket.rb
@@ -2,8 +2,6 @@
 # terms of the new BSD License.
 #
 # Copyright (c) 2008-2009, Sebastian Staudt
-#
-# $Id$
 
 require "byte_buffer"
 require "steam/sockets/steam_socket"
@@ -20,7 +18,7 @@ class SourceSocket < SteamSocket
     bytes_read = self.receive_packet 1400
     is_compressed = false
 
-    if @buffer.get_long == -2
+    if @buffer.get_long == 0xFFFFFFFE
       split_packets = Array.new
       begin
         # Parsing of split packet headers
@@ -51,7 +49,7 @@ class SourceSocket < SteamSocket
         else
           bytes_read = 0
         end
-      end while bytes_read > 0 && @buffer.get_long == -2
+      end while bytes_read > 0 && @buffer.get_long == 0xFFFFFFFE
       
       if is_compressed
         packet = SteamPacketFactory.reassemble_packet(split_packets, true, packet_checksum)

commit 87124be6aaf7aa305e032b7b692121f3f417d78a

Ruby: Fixed issues introduced in 17f210c0fab50464ff6526807b2c91efc53efcf9.
Closes #13

ruby/lib/byte_buffer.rb
@@ -2,8 +2,6 @@
 # terms of the new BSD License.
 #
 # Copyright (c) 2008, Sebastian Staudt
-#
-# $Id$
 
 require "exceptions/buffer_underflow_exception"
 
@@ -84,6 +82,10 @@ class ByteBuffer
 
   def get_short
     return self.get(2).unpack("v")[0]
+  end
+
+  def get_signed_long
+    return self.get(4).unpack('l')[0]
   end
   
   def get_string

ruby/lib/steam/packets/s2a_player_packet.rb
@@ -24,7 +24,7 @@ class S2A_PLAYER_Packet < SteamPacket
     @player_hash = {}
 
     while @content_data.remaining > 0
-      player_data = @content_data.get_byte, @content_data.get_string, @content_data.get_long, @content_data.get_float
+      player_data = @content_data.get_byte, @content_data.get_string, @content_data.get_signed_long, @content_data.get_float
       @player_hash[player_data[1]] = SteamPlayer.new(*player_data[0..3])
     end
   end

ruby/lib/steam/sockets/master_server_socket.rb
@@ -1,9 +1,7 @@
 # This code is free software; you can redistribute it and/or modify it under the
 # terms of the new BSD License.
 #
-# Copyright (c) 2008, Sebastian Staudt
-#
-# $Id$
+# Copyright (c) 2008-2009, Sebastian Staudt
 
 require "steam/sockets/steam_socket"
 
@@ -12,7 +10,7 @@ class MasterServerSocket < SteamSocket
   def get_reply
     self.receive_packet 1500
     
-    if @buffer.get_long != -1
+    if @buffer.get_long != 0xFFFFFFFF
       raise Exception.new("Master query response has wrong packet header.")
     end
     