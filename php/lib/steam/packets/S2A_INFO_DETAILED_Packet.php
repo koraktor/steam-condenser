b5f678946158a449d571a80d96735e5ef0f639b8
Sebastian Staudt <koraktor@gmail.com>
2009-06-08 07:59:25 +0200

    Fixed error on getting server info for some outdated "proxy" servers responding to a A2S_INFO request with S2A_INFO_DETAILED responses.

--- php/lib/steam/packets/S2A_INFO_DETAILED_Packet.php
+++ php/lib/steam/packets/S2A_INFO_DETAILED_Packet.php
@@ -3,54 +3,60 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
+ * Copyright (c) 2008-2009, Sebastian Staudt
+ *
  * @author Sebastian Staudt
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @package Steam Condenser (PHP)
  * @subpackage Packets
- * @version $Id$
  */
 
 require_once "steam/packets/S2A_INFO_BasePacket.php";
 
 /**
+ * The S2A_INFO_DETAILED_Packet class represents the response to a A2S_INFO
+ * request send to a GoldSrc server.
+ * This is deprecated by 10/24/2008 for GoldSrc servers. They use the same
+ * format as Source servers (S2A_INFO2) now.
+ *
  * @package Steam Condenser (PHP)
  * @subpackage Packets
  */
-class S2A_INFO_DETAILED_Packet extends S2A_INFO_BasePacket
-{
-	/**
-	 *
-	 */
-	public function __construct($data)
-	{
-		parent::__construct(SteamPacket::S2A_INFO_DETAILED_HEADER, $data);
+class S2A_INFO_DETAILED_Packet extends S2A_INFO_BasePacket {
 
-		$this->serverIp = $this->contentData->getString();
-		$this->serverName = $this->contentData->getString();
-		$this->mapName = $this->contentData->getString();
-		$this->gameDir = $this->contentData->getString();
-		$this->gameDescription = $this->contentData->getString();
-		$this->numberOfPlayers = $this->contentData->getByte();
-		$this->maxPlayers = $this->contentData->getByte();
-		$this->networkVersion = $this->contentData->getByte();
-		$this->dedicated = $this->contentData->getByte();
-		$this->operatingSystem = $this->contentData->getByte();
-		$this->passwordProtected = $this->contentData->getByte() == 1;
-		$this->isMod = $this->contentData->getByte() == 1;
+    public function __construct($data) {
+        parent::__construct(SteamPacket::S2A_INFO_DETAILED_HEADER, $data);
 
-		if($this->isMod)
-		{
-			$this->modInfo["urlInfo"] = $this->contentData->getString();
-			$this->modInfo["urlDl"] = $this->contentData->getString();
-			$this->contentData->getByte();
-			$this->modInfo["modVersion"] = $this->contentData->getLong();
-			$this->modInfo["modSize"] = $this->contentData->getLong();
-			$this->modInfo["svOnly"] = ($this->contentData->getByte() == 1);
-			$this->modInfo["clDll"] = ($this->contentData->getByte() == 1);
-		}
+        $this->serverIp = $this->contentData->getString();
+        $this->serverName = $this->contentData->getString();
+        $this->mapName = $this->contentData->getString();
+        $this->gameDir = $this->contentData->getString();
+        $this->gameDescription = $this->contentData->getString();
+        $this->numberOfPlayers = $this->contentData->getByte();
+        $this->maxPlayers = $this->contentData->getByte();
+        $this->networkVersion = $this->contentData->getByte();
+        $this->dedicated = $this->contentData->getByte();
+        $this->operatingSystem = $this->contentData->getByte();
+        $this->passwordProtected = $this->contentData->getByte() == 1;
+        $this->isMod = $this->contentData->getByte() == 1;
 
-		$this->secure = $this->contentData->getByte() == 1;
-		$this->numberOfBots = $this->contentData->getByte();
-	}
+        if($this->isMod) {
+            $this->modInfo["urlInfo"] = $this->contentData->getString();
+            $this->modInfo["urlDl"] = $this->contentData->getString();
+            $this->contentData->getByte();
+            if($this->contentData->remaining() == 12) {
+                $this->modInfo["modVersion"] = $this->contentData->getLong();
+                $this->modInfo["modSize"] = $this->contentData->getLong();
+                $this->modInfo["svOnly"] = ($this->contentData->getByte() == 1);
+                $this->modInfo["clDll"] = ($this->contentData->getByte() == 1);
+                $this->secure = $this->contentData->getByte() == 1;
+                $this->numberOfBots = $this->contentData->getByte();
+            }
+        }
+        else {
+            $this->secure = $this->contentData->getByte() == 1;
+            $this->numberOfBots = $this->contentData->getByte();
+        }
+    }
 }
 ?>
--- ruby/lib/steam/packets/s2a_info_detailed_packet.rb
+++ ruby/lib/steam/packets/s2a_info_detailed_packet.rb
@@ -1,14 +1,14 @@
 # This code is free software; you can redistribute it and/or modify it under the
 # terms of the new BSD License.
 #
-# Copyright (c) 2008, Sebastian Staudt
-#
-# $Id$
+# Copyright (c) 2008-2009, Sebastian Staudt
 
 require "steam/packets/s2a_info_base_packet"
 
 # The S2A_INFO_DETAILED_Packet class represents the response to a A2S_INFO
 # request send to a GoldSrc server.
+# This is deprecated by 10/24/2008 for GoldSrc servers. They use the same
+# format as Source servers (S2A_INFO2) now.
 class S2A_INFO_DETAILED_Packet < S2A_INFO_BasePacket
   
   # Creates a S2A_INFO_DETAILED response object based on the data received.
@@ -33,14 +33,18 @@ class S2A_INFO_DETAILED_Packet < S2A_INFO_BasePacket
       @mod_info["url_info"] = @content_data.get_string
       @mod_info["url_dl"] = @content_data.get_string
       @content_data.get_byte
-      @mod_info["mod_version"] = @content_data.get_long
-      @mod_info["mod_size"] = @content_data.get_long
-      @mod_info["sv_only"] = @content_data.get_byte == 1
-      @mod_info["cl_dll"] = @content_data.get_byte == 1
+      if @content_data.remaining == 12
+        @mod_info["mod_version"] = @content_data.get_long
+        @mod_info["mod_size"] = @content_data.get_long
+        @mod_info["sv_only"] = @content_data.get_byte == 1
+        @mod_info["cl_dll"] = @content_data.get_byte == 1
+        @secure = @content_data.get_byte == 1
+        @number_of_bots = @content_data.get_byte
+      end
+    else
+      @secure = @content_data.get_byte == 1
+      @number_of_bots = @content_data.get_byte
     end
-    
-    @secure = @content_data.get_byte == 1
-    @number_of_bots = @content_data.get_byte
   end
   
 end
