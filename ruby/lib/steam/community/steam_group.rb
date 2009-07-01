4a00df2a487d91f00946ea6365b6750bb27d4ab6
Sebastian Staudt <koraktor@gmail.com>
2009-07-01 21:33:58 +0200

    Ruby: Added caching to SteamGroup and SteamId
    This gives a huge performance boost, especially for groups with lots of members or players that are in lots of groups. Still needs some work as the cache doesn't check if the data has already been fetched.
    
    Also reworked the code of SteamId

--- ruby/lib/steam/community/steam_group.rb
+++ ruby/lib/steam/community/steam_group.rb
@@ -12,6 +12,32 @@ require 'steam/community/steam_id'
 class SteamGroup
 
   attr_reader :custom_url, :group_id64
+  
+  @@steam_groups = {}
+
+  # Returns whether the requested group is already cached
+  def self.cached?(id)
+    unless id.is_a? Numeric
+      id.downcase!
+    end
+    @@steam_groups.key?(id)
+  end
+
+  # Clears the group cache
+  def self.clear_cache
+    @@steam_groups = {}
+  end
+
+  # This checks the cache for an existing group. If it exists it is returned.
+  # Otherwise a new SteamGroup object is created.
+  # Overrides the default constructor.
+  def self.new(id, fetch = true)
+    if cached?(id)
+      @@steam_groups[id]
+    else
+      super(id, fetch)
+    end
+  end
 
   # Creates a SteamGroup object with the given group ID
   def initialize(id, fetch = false)
@@ -26,6 +52,20 @@ class SteamGroup
     rescue REXML::ParseException
       raise SteamCondenserException.new('SteamID could not be loaded.')
     end
+
+    cache
+  end
+
+  # Saves this group in the cache
+  def cache
+    unless @@steam_groups.key?(@group_id64)
+      @@steam_groups[@group_id64] = self
+      unless @custom_url.nil? or @@steam_groups.key?(@custom_url)
+        @@steam_groups[@custom_url] = self
+      end
+    end
+
+    true
   end
 
   # Returns the URL to the group's Steam Community page
@@ -47,6 +87,9 @@ class SteamGroup
       url = open("#{base_url}/memberslistxml?p=#{page}", {:proxy => true})
       @member_data = REXML::Document.new(url.read).root
 
+      if page == 1
+        @group_id64 = @member_data.elements['groupID64'].text.to_i
+      end
       total_pages = @member_data.elements['totalPages'].text.to_i
     
       @member_data.elements['members'].elements.each do |member|
--- ruby/lib/steam/community/steam_id.rb
+++ ruby/lib/steam/community/steam_id.rb
@@ -3,12 +3,12 @@
 #
 # Copyright (c) 2008-2009, Sebastian Staudt
 
-require "open-uri"
-require "rexml/document"
+require 'open-uri'
+require 'rexml/document'
 
-require "exceptions/steam_condenser_exception"
-require "steam/community/game_stats"
-require "steam/community/steam_group"
+require 'exceptions/steam_condenser_exception'
+require 'steam/community/game_stats'
+require 'steam/community/steam_group'
 
 # The SteamId class represents a Steam Community profile (also called Steam ID)
 class SteamId
@@ -19,23 +19,49 @@ class SteamId
               :real_name, :state_message, :steam_rating, :steam_rating_text,
               :summary, :vac_banned, :visibility_state
 
+  @@steam_ids = {}
+
+  # Returns whether the requested SteamID is already cached
+  def self.cached?(id)
+    unless id.is_a? Numeric
+      id.downcase!
+    end
+    @@steam_ids.key?(id)
+  end
+
+  # Clears the SteamID cache
+  def self.clear_cache
+    @@steam_ids = {}
+  end
+
   # Converts a SteamID as reported by game servers to a 64bit SteamID
   def self.convert_steam_id_to_community_id(steam_id)
-    if steam_id == "STEAM_ID_LAN"
-      raise SteamCondenserException.new("Cannot convert SteamID \"STEAM_ID_LAN\" to a community ID.")
+    if steam_id == 'STEAM_ID_LAN'
+      raise SteamCondenserException.new('Cannot convert SteamID "STEAM_ID_LAN" to a community ID.')
     elsif steam_id.match(/STEAM_[0-1]:[0-1]:[0-9]+/).nil?
       raise SteamCondenserException.new("SteamID \"#{steam_id}\" doesn't have the correct format.")
     end
     
-    steam_id = steam_id[6..-1].split(":").map!{|s| s.to_i}
+    steam_id = steam_id[6..-1].split(':').map!{|s| s.to_i}
     
-    return steam_id[1] + 76561197960265728 + steam_id[2] * 2
+    steam_id[1] + 76561197960265728 + steam_id[2] * 2
   end
 
   # Creates a new SteamId object using a SteamID64 converted from a server
   # SteamID
   def self.get_from_steam_id(steam_id)
-    return self.new(self.convert_steam_id_to_community_id(steam_id))
+    self.new(self.convert_steam_id_to_community_id(steam_id))
+  end
+
+  # This checks the cache for an existing SteamID. If it exists it is returned.
+  # Otherwise a new SteamID is created.
+  # Overrides the default constructor.
+  def self.new(id, fetch = true)
+    if cached?(id)
+      @@steam_ids[id]
+    else
+      super(id, fetch)
+    end
   end
 
   # Creates a new SteamId object for the given SteamID, either numeric or the
@@ -45,14 +71,16 @@ class SteamId
     if id.is_a? Numeric
       @steam_id64 = id
     else
-      @custom_url = id
+      @custom_url = id.downcase
     end
 
     begin
       self.fetch_data if fetch
     rescue REXML::ParseException
-      raise SteamCondenserException.new("SteamID could not be loaded.")
+      raise SteamCondenserException.new('SteamID could not be loaded.')
     end
+
+    cache
   end
 
   # Returns the base URL for this SteamID
@@ -63,61 +91,75 @@ class SteamId
       "http://steamcommunity.com/id/#{@custom_url}"
     end
   end
+
+  # Saves this SteamID in the cache
+  def cache
+    unless @@steam_ids.key?(@steam_id64)
+      @@steam_ids[@steam_id64] = self
+      unless @custom_url.nil? or @@steam_ids.key?(@custom_url)
+        @@steam_ids[@custom_url] = self
+      end
+    end
+
+    true
+  end
   
   # Fetchs data from the Steam Community by querying the XML version of the
   # profile specified by the ID of this SteamID
   def fetch_data
     profile_url = open(base_url + '?xml=1', {:proxy => true})
-    profile = REXML::Document.new(profile_url.read).elements["profile"]
+    profile = REXML::Document.new(profile_url.read).root
       
-    @image_url        = profile.elements["avatarIcon"].text[0..-5]
-    @online_state     = profile.elements["onlineState"].text
-    @privacy_state    = profile.elements["privacyState"].text
-    @state_message    = profile.elements["stateMessage"].text
-    @steam_id         = profile.elements["steamID"].text
-    @steam_id64       = profile.elements["steamID64"].text.to_i
-    @vac_banned       = (profile.elements["vacBanned"].text == 1)
-    @visibility_state = profile.elements["visibilityState"].text.to_i
+    @image_url        = profile.elements['avatarIcon'].text[0..-5]
+    @online_state     = profile.elements['onlineState'].text
+    @privacy_state    = profile.elements['privacyState'].text
+    @state_message    = profile.elements['stateMessage'].text
+    @steam_id         = profile.elements['steamID'].text
+    @steam_id64       = profile.elements['steamID64'].text.to_i
+    @vac_banned       = (profile.elements['vacBanned'].text == 1)
+    @visibility_state = profile.elements['visibilityState'].text.to_i
     
     # Only public profiles can be scanned for further information
-    if @privacy_state == "public"
-      @custom_url                       = profile.elements["customURL"].text
+    if @privacy_state == 'public'
+      @custom_url                       = profile.elements['customURL'].text.downcase
 
-      unless REXML::XPath.first(profile, "favoriteGame").nil?
-        @favorite_game                    = profile.elements["favoriteGame/name"].text
-        @favorite_game_hours_played       = profile.elements["favoriteGame/hoursPlayed2wk"].text
+      unless REXML::XPath.first(profile, 'favoriteGame').nil?
+        @favorite_game                    = profile.elements['favoriteGame/name'].text
+        @favorite_game_hours_played       = profile.elements['favoriteGame/hoursPlayed2wk'].text
       end
 
-      @head_line                        = profile.elements["headline"].text
-      @hours_played                     = profile.elements["hoursPlayed2Wk"].text.to_f
-      @location                         = profile.elements["location"].text
-      @member_since                     = Time.parse profile.elements["memberSince"].text
-      @real_name                        = profile.elements["realname"].text
-      @steam_rating                     = profile.elements["steamRating"].text.to_f
-      @summary                          = profile.elements["summary"].text
+      @head_line                        = profile.elements['headline'].text
+      @hours_played                     = profile.elements['hoursPlayed2Wk'].text.to_f
+      @location                         = profile.elements['location'].text
+      @member_since                     = Time.parse profile.elements['memberSince'].text
+      @real_name                        = profile.elements['realname'].text
+      @steam_rating                     = profile.elements['steamRating'].text.to_f
+      @summary                          = profile.elements['summary'].text
 
-      unless REXML::XPath.first(profile, "mostPlayedGames").nil?
+      unless REXML::XPath.first(profile, 'mostPlayedGames').nil?
         @most_played_games = Hash.new
-        profile.elements["mostPlayedGames"].elements.each("mostPlayedGame") do |most_played_game|
-          @most_played_games[most_played_game.elements["gameName"].text] = most_played_game.elements["hoursPlayed"].text.to_f
+        profile.elements['mostPlayedGames'].elements.each('mostPlayedGame') do |most_played_game|
+          @most_played_games[most_played_game.elements['gameName'].text] = most_played_game.elements['hoursPlayed'].text.to_f
         end
       end
       
       @friends = Array.new
-      profile.elements["friends"].elements.each("friend") do |friend|
-        @friends << SteamId.new(friend.elements["steamID64"].text.to_i, false)
+      profile.elements['friends'].elements.each('friend') do |friend|
+        @friends << SteamId.new(friend.elements['steamID64'].text.to_i, false)
       end
       
       @groups = Array.new
-      profile.elements["groups"].elements.each("group") do |group|
-        @groups << SteamGroup.new(group.elements["groupID64"].text.to_i)
+      profile.elements['groups'].elements.each('group') do |group|
+        @groups << SteamGroup.new(group.elements['groupID64'].text.to_i)
       end
       
       @links = Hash.new
-      profile.elements["weblinks"].elements.each("weblink") do |link|
-        @links[link.elements["title"].text] = link.elements["link"].text
+      profile.elements['weblinks'].elements.each('weblink') do |link|
+        @links[link.elements['title'].text] = link.elements['link'].text
       end
     end
+
+    true
   end
 
   # Fetches the games this user owns
@@ -147,12 +189,12 @@ class SteamId
       end
     end
 
-    return true
+    true
   end
   
   # Returns the URL of the full version of this user's avatar
   def full_avatar_url
-    return "#{@image_url}_full.jpg"
+    "#{@image_url}_full.jpg"
   end
   
   # Returns a GameStats object for the given game for the owner of this SteamID
@@ -180,27 +222,27 @@ class SteamId
   
   # Returns the URL of the icon version of this user's avatar
   def icon_url
-    return "#{@image_url}.jpg"
+    "#{@image_url}.jpg"
   end
   
   # Returns whether the owner of this SteamID is VAC banned
   def is_banned?
-    return @vac_banned 
+    @vac_banned 
   end
   
   # Returns whether the owner of this SteamId is playing a game
   def is_in_game?
-    return @online_state == "in-game"
+    @online_state == 'in-game'
   end
   
   # Returns whether the owner of this SteamID is currently logged into Steam
   def is_online?
-    return @online_state != "offline"
+    @online_state != 'offline'
   end
   
   # Returns the URL of the medium version of this user's avatar
   def medium_avatar_url
-    return "#{@image_url}_medium.jpg"
+    "#{@image_url}_medium.jpg"
   end
 
 end
