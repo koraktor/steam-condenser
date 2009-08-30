6e159c1dd09357f0d9dc862f1d0182db9f24af07
Sebastian Staudt <koraktor@gmail.com>
2009-08-30 19:56:23 +0200

    Ruby: ByteBuffer#get_string now return nil instead of an empty string when there is no null-terminated string

--- ruby/lib/byte_buffer.rb
+++ ruby/lib/byte_buffer.rb
@@ -1,7 +1,7 @@
 # This code is free software; you can redistribute it and/or modify it under the
 # terms of the new BSD License.
 #
-# Copyright (c) 2008, Sebastian Staudt
+# Copyright (c) 2008-2009, Sebastian Staudt
 
 require "exceptions/buffer_underflow_exception"
 
@@ -11,71 +11,71 @@ class ByteBuffer
   attr_accessor :limit
   attr_reader :position
   protected :initialize
-  
+
   # Creates a new buffer with the given size in bytes
   def self.allocate(length)
     return ByteBuffer.new(0.chr * length)
   end
-  
-  # Creates a new buffer from an existing String  
+
+  # Creates a new buffer from an existing String
   def self.wrap(byte_array)
     return ByteBuffer.new(byte_array.to_s)
   end
-  
-  # 
+
+  #
   def initialize(byte_array)
     if byte_array.is_a? ByteBuffer
       raise Exception.new
     end
-    
+
     @byte_array = byte_array.to_s
     @capacity = @byte_array.length
     @limit = @capacity
     @position = 0
     @mark = -1
   end
-  
+
   def array
     return @byte_array
   end
-  
+
   def clear
     @limit = @capacity
     @position = 0
     @mark = -1
-    
+
     return self
   end
-  
+
   def flip
     @limit = @position
     @position = 0
     @mark = -1
-    
+
     return self
   end
-  
+
   def get(length = nil)
     if length == nil
       length = @limit - @position
     elsif length > self.remaining
       BufferUnderflowException.new
     end
-    
+
     data = @byte_array[@position, length]
     @position += length
-    
+
     return data
   end
-  
+
   def get_byte
     return self.get(1)[0]
   end
-  
+
   def get_float
     return self.get(4).unpack("e")[0]
   end
-  
+
   def get_long
     return self.get(4).unpack("V")[0]
   end
@@ -87,39 +87,39 @@ class ByteBuffer
   def get_signed_long
     return self.get(4).unpack('l')[0]
   end
-  
+
   def get_string
     zero_byte_index = @byte_array.index("\0", @position)
     if zero_byte_index == nil
-      return ""
+      nil
     else
       data_string = self.get(zero_byte_index - @position)
-      @position += 1    
-      return data_string
+      @position += 1
+      data_string
     end
   end
-  
+
   def length
     return @byte_array.length
   end
-  
+
   def remaining
     return @limit - @position
   end
-  
+
   def rewind
     @position = 0
     @mark = -1
-    
+
     return self
   end
-  
+
   def put(source_byte_array)
     new_position = [source_byte_array.length, self.remaining].min
 
     @byte_array[@position, new_position] = source_byte_array
     @position = new_position
-    
+
     return self
   end
 
