4763f9ba26d8a440769d78ed410bcb2877ed7049
Sebastian Staudt <koraktor@gmail.com>
2009-09-11 13:55:21 +0200

    PHP: Updated Steam Community tests

--- php/test/SteamCommunityTests.php
+++ php/test/SteamCommunityTests.php
@@ -8,7 +8,6 @@
  * @author  Sebastian Staudt
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @package Steam Condenser (PHP)
- * @version $Id$
  */
 
 ini_set("include_path", ini_get("include_path") . PATH_SEPARATOR . dirname(__FILE__) . "/../lib");
@@ -22,20 +21,38 @@ require_once "PHPUnit/Framework.php";
  * @package    Steam Condenser (PHP)
  * @subpackage Tests
  */
-class SteamCommunityTests extends PHPUnit_Framework_TestCase
-{
-	public function testSteamIdByCustomUrl()
-	{
-		$steamId = new SteamId("Koraktor");
-		print_r($steamId);
-                print_r($steamId->getGameStats("TF2"));
-	}
+class SteamCommunityTests extends PHPUnit_Framework_TestCase {
 
-        public function testSteamIdBySteamId64()
-	{
-		$steamId = new SteamId(76561197961384956);
-		print_r($steamId);
-                print_r($steamId->getGameStats("TF2"));
-	}
+    public function testGroupByCustomUrl() {
+        $group = new SteamGroup('valve');
+        print_r($group->getMemberCount());
+    }
+
+    public function testGroupByGroupId64() {
+        $group = new SteamGroup('103582791429521412');
+        print_r($group->getMemberCount());
+    }
+
+    public function testSteamIdByCustomUrl() {
+        $steamId = new SteamId('koraktor');
+        $s = $steamId->getGameStats('L4D');
+        print_r($s->getSurvivalStats());
+        print_r($s->getTeamplayStats());
+        print_r($s->getVersusStats());
+        print_r($s->getWeaponStats());
+    }
+
+    public function testSteamIdBySteamId64() {
+        $steamId = new SteamId('76561197961384956');
+        print_r($steamId);
+        print_r($steamId->getGameStats('L4D'));
+        print_r($steamId->getGameStats('DoD:S'));
+        print_r($steamId->getGameStats('TF2'));
+    }
+
+    public function testGamesParser() {
+        $steamId = new SteamId('koraktor', false);
+        print_r($steamId->getGames());
+    }
 }
 ?>
