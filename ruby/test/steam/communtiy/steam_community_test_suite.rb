3f81912935697c7d62948b3ae7a083dd7b926e66
Sebastian Staudt <koraktor@gmail.com>
2010-04-07 20:21:31 +0200

    Ruby: Fixed Steam Community test suite load path

--- ruby/test/steam/communtiy/steam_community_test_suite.rb
+++ ruby/test/steam/communtiy/steam_community_test_suite.rb
@@ -1,9 +1,12 @@
 # This code is free software; you can redistribute it and/or modify it under the
 # terms of the new BSD License.
 #
-# Copyright (c) 2009, Sebastian Staudt
+# Copyright (c) 2009-2010, Sebastian Staudt
 
-require 'test/unit' 
+require 'test/unit'
+
+suite_dir = File.dirname(__FILE__)
+$LOAD_PATH.unshift(suite_dir) unless $LOAD_PATH.include?(suite_dir)
 
 require 'steam_group_tests'
 require 'steam_id_tests'
