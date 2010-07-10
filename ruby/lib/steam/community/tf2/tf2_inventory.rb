97da5698c351b4c852d3a1c25ee2ceab1ab40a15
Sebastian Staudt <koraktor@gmail.com>
2010-07-10 14:46:01 +0200

    Ruby: Made TF2Inventory a Cacheable

--- ruby/lib/steam/community/tf2/tf2_inventory.rb
+++ ruby/lib/steam/community/tf2/tf2_inventory.rb
@@ -3,18 +3,37 @@
 #
 # Copyright (c) 2010, Sebastian Staudt
 
+require 'steam/community/cacheable'
 require 'steam/community/tf2/tf2_item'
 require 'steam/community/web_api'
 
 # Represents the inventory (aka. Backpack) of a Team Fortress 2 player
 class TF2Inventory
 
+  include Cacheable
+  cacheable_with_ids :steam_id64
+
   include WebApi
 
-  attr_reader :items
+  attr_reader :items, :steam_id64
 
-  # Creates a new inventory object for the given SteamID64
-  def initialize(steam_id)
+  # Creates a new inventory object for the given SteamID64. This calls update
+  # to fetch the data and create the TF2Item instances contained in this
+  # players backpack
+  def initialize(steam_id64, fetch_now = true)
+    @steam_id64 = steam_id64
+
+    super(fetch_now)
+  end
+
+  # Returns the item at the given position in the backpack. The positions range
+  # from 1 to 100 instead of the usual array indices (0 to 99).
+  def [](index)
+    @items[index - 1]
+  end
+
+  # Updates the contents of the backpack using Steam Web API
+  def fetch
     result = json!('ITFItems_440', 'GetPlayerItems', 1, { :SteamID => @steam_id64 })
 
     @items = []
@@ -24,11 +43,6 @@ class TF2Inventory
         @items[item.backpack_position - 1] = item
       end
     end
-  end
-
-  # Returns the item at the given position in the backpack
-  def [](index)
-    @items[index]
   end
 
   # Returns the number of items in the user's backpack
