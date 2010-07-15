837dd2625232caff68e5125f36dc782cb8c8d13a
Sebastian Staudt <koraktor@gmail.com>
2010-07-15 21:59:18 +0200

    Java / PHP: Added TF2Stats#getInventory()
    Includes some other minor changes in TF2Stats

--- java/src/main/java/com/github/koraktor/steamcondenser/steam/community/tf2/TF2Stats.java
+++ java/src/main/java/com/github/koraktor/steamcondenser/steam/community/tf2/TF2Stats.java
@@ -2,17 +2,20 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
- * Copyright (c) 2008-2009, Sebastian Staudt
+ * Copyright (c) 2008-2010, Sebastian Staudt
  */
 
 package com.github.koraktor.steamcondenser.steam.community.tf2;
 
 import java.util.ArrayList;
 
+import org.json.JSONException;
+
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
+import com.github.koraktor.steamcondenser.exceptions.WebApiException;
 import com.github.koraktor.steamcondenser.steam.community.GameStats;
 
 /**
@@ -23,16 +26,19 @@ import com.github.koraktor.steamcondenser.steam.community.GameStats;
  */
 public class TF2Stats extends GameStats {
 
-	private int accumulatedPoints;
-	private ArrayList<TF2Class> classStats;
+    private int accumulatedPoints;
+
+    private ArrayList<TF2Class> classStats;
+
+    private TF2Inventory inventory;
 
 	/**
 	 * Creates a new object holding Team Fortress 2 statistics for the given
 	 * user
-	 * @param steamId
-	 * @throws ParserConfigurationException
-	 * @throws SAXException
-	 * @throws IOException
+
+     * @param steamId The custom URL or 64bit Steam ID of the user
+     * @throws SteamCondenserException If an error occurs while fetching the
+     *                                 stats data
 	 */
 	public TF2Stats(Object steamId)
 			throws SteamCondenserException {
@@ -42,13 +48,18 @@ public class TF2Stats extends GameStats {
 			this.accumulatedPoints = Integer.parseInt(((Element) this.xmlData.getElementsByTagName("stats").item(0)).getElementsByTagName("accumulatedPoints").item(0).getTextContent());
 		}
 	}
-
+    /**
+     * Returns the total points this player has achieved in his career
+     *
+     * @return This player's accumulated points
+     */
 	public int getAccumulatedPoints() {
 		return this.accumulatedPoints;
 	}
 
 	/**
 	 * Returns the statistics for all Team Fortress 2 classes for this user
+     *
 	 * @return An array storing individual TF2Class objects for each Team
 	 *         Fortress 2 class
 	 */
@@ -67,4 +78,26 @@ public class TF2Stats extends GameStats {
 
 		return this.classStats;
 	}
+
+    /**
+     * Returns the current Team Fortress 2 inventory (a.k.a. backpack) of this
+     * player
+     *
+     * @return This player's TF2 backpack
+     * @throws JSONException If the received data could not be parsed
+     * @throws WebApiException If an error occured while querying Steam's Web
+     *                         API
+     */
+    public TF2Inventory getInventory()
+            throws JSONException, WebApiException {
+        if(!this.isPublic()) {
+            return null;
+        }
+
+        if(this.inventory == null) {
+            this.inventory = TF2Inventory.create(this.steamId64);
+        }
+
+        return this.inventory;
+    }
 }
--- php/lib/steam/community/tf2/TF2Stats.php
+++ php/lib/steam/community/tf2/TF2Stats.php
@@ -3,7 +3,7 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
- * Copyright (c) 2008-2009, Sebastian Staudt
+ * Copyright (c) 2008-2010, Sebastian Staudt
  *
  * @author     Sebastian Staudt
  * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
@@ -13,6 +13,7 @@
 
 require_once STEAM_CONDENSER_PATH . 'steam/community/GameStats.php';
 require_once STEAM_CONDENSER_PATH . 'steam/community/tf2/TF2ClassFactory.php';
+require_once STEAM_CONDENSER_PATH . 'steam/community/tf2/TF2Inventory.php';
 
 /**
  * The TF2Stats class represents the game statistics for a single user in Team
@@ -23,9 +24,25 @@ require_once STEAM_CONDENSER_PATH . 'steam/community/tf2/TF2ClassFactory.php';
 class TF2Stats extends GameStats {
 
     /**
+     * @var int
+     */
+    private $accumulatedPoints;
+
+    /**
+     * @var array
+     */
+    private $classStats;
+
+    /**
+     * @var TF2Inventory
+     */
+    private $inventory;
+
+    /**
      * Creates a TF2Stats object by calling the super constructor with the game
      * name "TF2"
-     * @param $steamId
+     *
+     * @param $steamId The custom URL or 64bit Steam ID of the user
      */
     public function __construct($steamId) {
         parent::__construct($steamId, "TF2");
@@ -36,8 +53,20 @@ class TF2Stats extends GameStats {
     }
 
     /**
+     * Returns the total points this player has achieved in his career
+     *
+     * @return int This player's accumulated points
+     */
+    public function getAccumulatedPoints() {
+        return $this->accumulatedPoints;
+    }
+
+    /**
      * Returns a Hash of TF2Class for this user containing all Team Fortress 2
      * classes. If the classes haven't been parsed already, parsing is done now.
+     *
+     * @return array An array storing individual TF2Class objects for each Team
+     *               Fortress 2 class
      */
     public function getClassStats() {
         if(!$this->isPublic()) {
@@ -51,6 +80,24 @@ class TF2Stats extends GameStats {
         }
 
         return $this->classStats;
+    }
+
+    /**
+     * Returns the current Team Fortress 2 inventory (a.k.a. backpack) of this
+     * player
+     *
+     * @return TF2Inventory This player's TF2 backpack
+     */
+    public function getInventory() {
+        if(!$this->isPublic()) {
+            return;
+        }
+
+        if(empty($this->inventory)) {
+            $this->inventory = TF2Inventory::create($this->steamId64);
+        }
+
+        return $this->steamId64;
     }
 }
 ?>
