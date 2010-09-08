66fe5d885128538ff9283df1a5ba340f7d444245
Sebastian Staudt <koraktor@gmail.com>
2010-09-08 12:08:24 +0200

    Ruby / PHP: Version bump to 0.11.2

--- php/lib/steam-condenser.php
+++ php/lib/steam-condenser.php
@@ -11,7 +11,7 @@
  */
 
 define(STEAM_CONDENSER_PATH, dirname(__FILE__) . '/');
-define(STEAM_CONDENSER_VERSION, '0.11.1');
+define(STEAM_CONDENSER_VERSION, '0.11.2');
 
 require_once STEAM_CONDENSER_PATH . 'steam/servers/GoldSrcServer.php';
 require_once STEAM_CONDENSER_PATH . 'steam/servers/MasterServer.php';
