dac171c6d4314e94c229eaf54e20b8a029fd5a52
Sebastian Staudt <koraktor@gmail.com>
2011-01-01 08:25:27 +0100

    PHP: Added documentation for M2C_ISVALIDMD5_Packet

--- php/lib/steam/packets/M2C_ISVALIDMD5_Packet.php
+++ php/lib/steam/packets/M2C_ISVALIDMD5_Packet.php
@@ -3,7 +3,7 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
- * Copyright (c) 2010, Sebastian Staudt
+ * Copyright (c) 2010-2011, Sebastian Staudt
  *
  * @author     Sebastian Staudt
  * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
@@ -14,16 +14,25 @@
 require_once STEAM_CONDENSER_PATH . 'steam/packets/SteamPacket.php';
 
 /**
+ * A packet of type M2S_ISVALIDMD5 is used by the master server to provide a
+ * challenge number to a game server
+ *
  * @package Steam Condenser (PHP)
  * @subpackage Packets
  */
 class M2C_ISVALIDMD5_Packet extends SteamPacket
 {
+
     /**
      * @var int
      */
     private $challenge;
 
+    /**
+     * Creates a new response packet with the data from the master server
+     *
+     * @param string $data The packet data sent by the master server
+     */
     public function __construct($data) {
         parent::__construct(SteamPacket::M2C_ISVALIDMD5_HEADER, $data);
 
@@ -31,6 +40,11 @@ class M2C_ISVALIDMD5_Packet extends SteamPacket
         $this->challenge = $this->contentData->getUnsignedLong();
     }
 
+    /**
+     * Returns the challenge number to use for master server communication
+     *
+     * @return long The challenge number
+     */
     public function getChallenge() {
         return $this->challenge;
     }
