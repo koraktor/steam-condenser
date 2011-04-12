0da1323a3cabb1bdef43745ff3ed75a52d41c432
Sebastian Staudt <koraktor@gmail.com>
2011-04-12 23:21:55 +0200

    Version bump to 0.13.0

--- php/lib/steam-condenser.php
+++ php/lib/steam-condenser.php
@@ -3,7 +3,7 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
- * Copyright (c) 2010, Sebastian Staudt
+ * Copyright (c) 2010-2011, Sebastian Staudt
  *
  * @author Sebastian Staudt
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
@@ -11,7 +11,7 @@
  */
 
 define('STEAM_CONDENSER_PATH', dirname(__FILE__) . '/');
-define('STEAM_CONDENSER_VERSION', '0.12.0');
+define('STEAM_CONDENSER_VERSION', '0.13.0');
 
 require_once STEAM_CONDENSER_PATH . 'steam/servers/GoldSrcServer.php';
 require_once STEAM_CONDENSER_PATH . 'steam/servers/MasterServer.php';
--- ruby/lib/steam-condenser/version.rb
+++ ruby/lib/steam-condenser/version.rb
@@ -1,10 +1,10 @@
 # This code is free software; you can redistribute it and/or modify it under the
 # terms of the new BSD License.
 #
-# Copyright (c) 2010, Sebastian Staudt
+# Copyright (c) 2010-2011, Sebastian Staudt
 
 module SteamCondenser
 
-  VERSION = '0.12.0'
+  VERSION = '0.13.0'
 
 end
