0d020e77f12d524897eeae50942acb7ddb6411b0
Sebastian Staudt <koraktor@gmail.com>
2011-06-01 23:06:32 +0200

    Version bump to 0.14.0

--- php/lib/steam-condenser.php
+++ php/lib/steam-condenser.php
@@ -11,7 +11,7 @@
  */
 
 define('STEAM_CONDENSER_PATH', dirname(__FILE__) . '/');
-define('STEAM_CONDENSER_VERSION', '0.13.1');
+define('STEAM_CONDENSER_VERSION', '0.14.0');
 
 require_once STEAM_CONDENSER_PATH . 'steam/servers/GoldSrcServer.php';
 require_once STEAM_CONDENSER_PATH . 'steam/servers/MasterServer.php';
--- ruby/lib/steam-condenser/version.rb
+++ ruby/lib/steam-condenser/version.rb
@@ -6,6 +6,6 @@
 module SteamCondenser
 
   # The current version of Steam Condenser
-  VERSION = '0.13.1'
+  VERSION = '0.14.0'
 
 end
