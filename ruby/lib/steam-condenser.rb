c3e1343a563e3a14affa75df8f31e8b093ceed25
Sebastian Staudt <koraktor@gmail.com>
2011-06-03 21:29:40 +0200

    Ruby: Require Bundler so it works out of the box

--- ruby/lib/steam-condenser.rb
+++ ruby/lib/steam-condenser.rb
@@ -3,6 +3,8 @@
 #
 # Copyright (c) 2009-2011, Sebastian Staudt
 
+require 'bundler'
+
 # This module is used as a wrapper around Steam Condenser's classes
 #
 # It does not provide any own functionality, but this file is used as an entry
