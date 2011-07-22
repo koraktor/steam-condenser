e9c9130e86a545333d9939ca0fc76e86cd479efc
Sebastian Staudt <koraktor@gmail.com>
2011-07-22 10:07:31 +0200

    PHP: Fixed variable name typo in TF2Class

--- php/lib/steam/community/tf2/TF2Class.php
+++ php/lib/steam/community/tf2/TF2Class.php
@@ -3,7 +3,7 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
- * Copyright (c) 2008-2009, Sebastian Staudt
+ * Copyright (c) 2008-2011, Sebastian Staudt
  *
  * @author     Sebastian Staudt
  * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
@@ -32,7 +32,7 @@ class TF2Class extends GameClass {
         $this->maxDefenses           = (int)    $classData->ipointdefenses;
         $this->maxDominations        = (int)    $classData->idominations;
         $this->maxKillAssists        = (int)    $classData->ikillassists;
-        $this->maxKils               = (int)    $classData->inumberofkills;
+        $this->maxKills              = (int)    $classData->inumberofkills;
         $this->maxRevenges           = (int)    $classData->irevenge;
         $this->maxScore              = (int)    $classData->ipointsscored;
         $this->maxTimeAlive          = (int)    $classData->iplaytime;
