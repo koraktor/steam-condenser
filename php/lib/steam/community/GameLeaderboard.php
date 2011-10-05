fe1f657fd6ea16af51000b160543e70025736da2
Sebastian Staudt <koraktor@gmail.com>
2011-10-05 20:56:27 +0200

    PHP: Fixed copyright notices for leaderboard files

--- php/lib/steam/community/GameLeaderboard.php
+++ php/lib/steam/community/GameLeaderboard.php
@@ -3,7 +3,7 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
- * Copyright (c) 2008-2011, Sebastian Staudt
+ * Copyright (c) 2011, Nicholas Hastings
  *
  * @author     Sebastian Staudt
  * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
