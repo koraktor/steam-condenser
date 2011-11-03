966ed5f8041a43a9809fde8cc483c9617b57d8d0
Sebastian Staudt <koraktor@gmail.com>
2011-11-03 07:15:35 +0100

    Ruby: Cleaned up whitespaces in test_steam_socket.rb

--- ruby/test/steam/sockets/test_steam_socket.rb
+++ ruby/test/steam/sockets/test_steam_socket.rb
@@ -27,71 +27,71 @@ class TestSteamSocket < Test::Unit::TestCase
     should 'create and connect an UDP socket' do
       socket = mock
       socket.expects(:connect).with '127.0.0.1', 27015
-      UDPsocket.expects(:new).returns socket
+      UDPSocket.expects(:new).returns socket
 
       GenericSteamSocket.new '127.0.0.1'
     end
 
   end
-  
+
   context 'A socket' do
-  
+
     setup do
       @udp_socket = mock
       @udp_socket.stubs(:connect).with '127.0.0.1', 27015
-      UDPsocket.stubs(:new).returns @udp_socket
+      UDPSocket.stubs(:new).returns @udp_socket
 
       @socket = GenericSteamSocket.new '127.0.0.1'
     end
-    
+
     should 'close the UDP socket if it is closed' do
       @udp_socket.expects :close
-      
+
       @socket.close
     end
-    
+
     should 'send packet data using the UDP socket' do
       packet = mock
       packet.expects(:to_s).returns 'test'
       @udp_socket.expects(:send).with 'test', 0
-      
+
       @socket.send packet
     end
-    
+
     should 'raise a timeout if no reply is received' do
       @socket.expects(:select).with([@udp_socket], nil, nil, 1).returns nil
-      
+
       assert_raises SteamCondenser::TimeoutError do
         @socket.receive_packet
       end
     end
-    
+
     should 'receive a packet into a new buffer' do
       @socket.expects(:select).with([@udp_socket], nil, nil, 1).returns true
       @udp_socket.expects(:recv).with(4).returns 'test'
-      
+
       assert_equal 4, @socket.receive_packet(4)
-      
+
       buffer = @socket.instance_variable_get :@buffer
       assert_equal 0, buffer.pos
       assert_equal 4, buffer.size
       assert_equal 'test', buffer.string
     end
-    
+
     should 'receive a packet into an existing buffer' do
       @socket.instance_variable_set :@buffer, StringIO.alloc(10)
-    
+
       @socket.expects(:select).with([@udp_socket], nil, nil, 1).returns true
       @udp_socket.expects(:recv).with(10).returns 'test'
-      
+
       assert_equal 4, @socket.receive_packet
-      
+
       buffer = @socket.instance_variable_get :@buffer
       assert_equal 0, buffer.pos
       assert_equal 4, buffer.size
       assert_equal 'test', buffer.string
     end
-  
+
   end
 
 end
